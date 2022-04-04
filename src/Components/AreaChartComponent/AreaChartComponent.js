4ad7c4660d45b1fe9face93983484ec4c36884aa
asaduzzaman599 <asaduzzamansoumit@gmail.com>
2022-04-04 14:48:06 +0600

    area chart added

--- src/Components/AreaChartComponent/AreaChartComponent.js
+++ src/Components/AreaChartComponent/AreaChartComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AreaChart } from 'recharts';
+import { Area, AreaChart, CartesianGrid, Tooltip, XAxis, YAxis } from 'recharts';
 
 const AreaChartComponent = ({data}) => {
     return (
@@ -19,9 +19,9 @@ const AreaChartComponent = ({data}) => {
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
-          <Area type="monotone" dataKey="uv" stackId="1" stroke="#8884d8" fill="#8884d8" />
-          <Area type="monotone" dataKey="pv" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
-          <Area type="monotone" dataKey="amt" stackId="1" stroke="#ffc658" fill="#ffc658" />
+          <Area type="monotone" dataKey="sell" stackId="1" stroke="#8884d8" fill="#8884d8" />
+          <Area type="monotone" dataKey="investment" stackId="1" stroke="#8884d8" fill="#8884d8" />
+          <Area type="monotone" dataKey="revenue" stackId="1" stroke="#B6FFCE" fill="#B6FFCE" />
         </AreaChart>
       </div>
     );
